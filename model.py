a9fde7068be94a8651a96d54e67d772934d5abf9
mxl1990 <mxl1990@gmail.com>
2017-09-28 15:25:31 +0800

    change the generator function with respect to f function in generator change the loss which is deprecated modify some definition that cause error in optimizer

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
+#import model file
+from model3 import DCGAN, conv_out_size_same
+from ops import *
+from utils import *
+
 
 class DTN(object):
     """Domain Transfer Network
@@ -9,52 +14,52 @@ class DTN(object):
         self.mode = mode
         self.learning_rate = learning_rate
         
-    def content_extractor(self, images, reuse=False):
-        # images: (batch, 32, 32, 3) or (batch, 32, 32, 1)
+    # def content_extractor(self, images, reuse=False):
+    #     # images: (batch, 32, 32, 3) or (batch, 32, 32, 1)
         
-        if images.get_shape()[3] == 1:
-            # For mnist dataset, replicate the gray scale image 3 times.
-            images = tf.image.grayscale_to_rgb(images)
+    #     if images.get_shape()[3] == 1:
+    #         # For mnist dataset, replicate the gray scale image 3 times.
+    #         images = tf.image.grayscale_to_rgb(images)
         
-        with tf.variable_scope('content_extractor', reuse=reuse):
-            with slim.arg_scope([slim.conv2d], padding='SAME', activation_fn=None,
-                                 stride=2,  weights_initializer=tf.contrib.layers.xavier_initializer()):
-                with slim.arg_scope([slim.batch_norm], decay=0.95, center=True, scale=True, 
-                                    activation_fn=tf.nn.relu, is_training=(self.mode=='train' or self.mode=='pretrain')):
+    #     with tf.variable_scope('content_extractor', reuse=reuse):
+    #         with slim.arg_scope([slim.conv2d], padding='SAME', activation_fn=None,
+    #                              stride=2,  weights_initializer=tf.contrib.layers.xavier_initializer()):
+    #             with slim.arg_scope([slim.batch_norm], decay=0.95, center=True, scale=True, 
+    #                                 activation_fn=tf.nn.relu, is_training=(self.mode=='train' or self.mode=='pretrain')):
                     
-                    net = slim.conv2d(images, 64, [3, 3], scope='conv1')   # (batch_size, 16, 16, 64)
-                    net = slim.batch_norm(net, scope='bn1')
-                    net = slim.conv2d(net, 128, [3, 3], scope='conv2')     # (batch_size, 8, 8, 128)
-                    net = slim.batch_norm(net, scope='bn2')
-                    net = slim.conv2d(net, 256, [3, 3], scope='conv3')     # (batch_size, 4, 4, 256)
-                    net = slim.batch_norm(net, scope='bn3')
-                    net = slim.conv2d(net, 128, [4, 4], padding='VALID', scope='conv4')   # (batch_size, 1, 1, 128)
-                    net = slim.batch_norm(net, activation_fn=tf.nn.tanh, scope='bn4')
-                    if self.mode == 'pretrain':
-                        net = slim.conv2d(net, 10, [1, 1], padding='VALID', scope='out')
-                        net = slim.flatten(net)
-                    return net
+    #                 net = slim.conv2d(images, 64, [3, 3], scope='conv1')   # (batch_size, 16, 16, 64)
+    #                 net = slim.batch_norm(net, scope='bn1')
+    #                 net = slim.conv2d(net, 128, [3, 3], scope='conv2')     # (batch_size, 8, 8, 128)
+    #                 net = slim.batch_norm(net, scope='bn2')
+    #                 net = slim.conv2d(net, 256, [3, 3], scope='conv3')     # (batch_size, 4, 4, 256)
+    #                 net = slim.batch_norm(net, scope='bn3')
+    #                 net = slim.conv2d(net, 128, [4, 4], padding='VALID', scope='conv4')   # (batch_size, 1, 1, 128)
+    #                 net = slim.batch_norm(net, activation_fn=tf.nn.tanh, scope='bn4')
+    #                 if self.mode == 'pretrain':
+    #                     net = slim.conv2d(net, 10, [1, 1], padding='VALID', scope='out')
+    #                     net = slim.flatten(net)
+    #                 return net
                 
     def generator(self, inputs, reuse=False):
         # inputs: (batch, 1, 1, 128)
-        with tf.variable_scope('generator', reuse=reuse):
+        with tf.variable_scope('Generator', reuse=reuse):
             with slim.arg_scope([slim.conv2d_transpose], padding='SAME', activation_fn=None,           
                                  stride=2, weights_initializer=tf.contrib.layers.xavier_initializer()):
                 with slim.arg_scope([slim.batch_norm], decay=0.95, center=True, scale=True, 
                                      activation_fn=tf.nn.relu, is_training=(self.mode=='train')):
 
-                    net = slim.conv2d_transpose(inputs, 512, [4, 4], padding='VALID', scope='conv_transpose1')   # (batch_size, 4, 4, 512)
+                    net = slim.conv2d_transpose(inputs, 64*4, [5, 5], scope='conv_transpose1')   # (batch_size, 4, 4, 512)
                     net = slim.batch_norm(net, scope='bn1')
-                    net = slim.conv2d_transpose(net, 256, [3, 3], scope='conv_transpose2')  # (batch_size, 8, 8, 256)
+                    net = slim.conv2d_transpose(net, 64*2, [5, 5], scope='conv_transpose2')  # (batch_size, 8, 8, 256)
                     net = slim.batch_norm(net, scope='bn2')
-                    net = slim.conv2d_transpose(net, 128, [3, 3], scope='conv_transpose3')  # (batch_size, 16, 16, 128)
+                    net = slim.conv2d_transpose(net, 64, [5, 5], scope='conv_transpose3')  # (batch_size, 16, 16, 128)
                     net = slim.batch_norm(net, scope='bn3')
-                    net = slim.conv2d_transpose(net, 1, [3, 3], activation_fn=tf.nn.tanh, scope='conv_transpose4')   # (batch_size, 32, 32, 1)
+                    net = slim.conv2d_transpose(net, 3, [5, 5], scope='conv_transpose4')   # (batch_size, 32, 32, 1)
                     return net
     
     def discriminator(self, images, reuse=False):
         # images: (batch, 32, 32, 1)
-        with tf.variable_scope('discriminator', reuse=reuse):
+        with tf.variable_scope('Discriminator', reuse=reuse):
             with slim.arg_scope([slim.conv2d], padding='SAME', activation_fn=None,
                                  stride=2,  weights_initializer=tf.contrib.layers.xavier_initializer()):
                 with slim.arg_scope([slim.batch_norm], decay=0.95, center=True, scale=True, 
@@ -70,7 +75,7 @@ class DTN(object):
                     net = slim.flatten(net)
                     return net
                 
-    def build_model(self):
+    def build_model(self, sess):
         
         if self.mode == 'pretrain':
             self.images = tf.placeholder(tf.float32, [None, 32, 32, 3], 'svhn_images')
@@ -93,25 +98,38 @@ class DTN(object):
             self.summary_op = tf.summary.merge([loss_summary, accuracy_summary])
 
         elif self.mode == 'eval':
-            self.images = tf.placeholder(tf.float32, [None, 32, 32, 3], 'svhn_images')
+            self.images = tf.placeholder(tf.float32, [64, 96, 96, 3], 'source_images1')
 
             # source domain (svhn to mnist)
-            self.fx = self.content_extractor(self.images)
+            dcgan = DCGAN(sess, input_height=96, input_width=96, is_crop=False,
+                 batch_size=64, sample_num = 64, output_height=96, output_width=96)
+
+            _, _, self.fx = dcgan.discriminator(self.images, reuse=True)
             self.sampled_images = self.generator(self.fx)
 
         elif self.mode == 'train':
-            self.src_images = tf.placeholder(tf.float32, [None, 32, 32, 3], 'svhn_images')
-            self.trg_images = tf.placeholder(tf.float32, [None, 32, 32, 1], 'mnist_images')
+            self.src_images = tf.placeholder(tf.float32, [64, 96, 96, 3], 'source_images')
+            self.trg_images = tf.placeholder(tf.float32, [64, 96, 96, 3], 'target_images')
+            dcgan = DCGAN(sess, input_height=96, input_width=96, is_crop=False,
+                 batch_size=64, sample_num = 64, output_height=96, output_width=96)
             
             # source domain (svhn to mnist)
-            self.fx = self.content_extractor(self.src_images)
+            # self.fx = self.content_extractor(self.src_images)
+            _, _, self.fx = dcgan.discriminator(self.src_images, reuse=True)
+
             self.fake_images = self.generator(self.fx)
+            print(self.fake_images)
+
             self.logits = self.discriminator(self.fake_images)
-            self.fgfx = self.content_extractor(self.fake_images, reuse=True)
+            _, _, self.fgfx = dcgan.discriminator(self.fake_images, reuse=True)
 
             # loss
-            self.d_loss_src = slim.losses.sigmoid_cross_entropy(self.logits, tf.zeros_like(self.logits))
-            self.g_loss_src = slim.losses.sigmoid_cross_entropy(self.logits, tf.ones_like(self.logits))
+            # self.d_loss_src = slim.losses.sigmoid_cross_entropy(self.logits, tf.zeros_like(self.logits))
+            # self.g_loss_src = slim.losses.sigmoid_cross_entropy(self.logits, tf.ones_like(self.logits))
+
+            self.d_loss_src = tf.losses.sigmoid_cross_entropy(logits=self.logits, multi_class_labels=tf.zeros_like(self.logits))
+            self.g_loss_src = tf.losses.sigmoid_cross_entropy(logits=self.logits, multi_class_labels=tf.ones_like(self.logits))
+
             self.f_loss_src = tf.reduce_mean(tf.square(self.fx - self.fgfx)) * 15.0
             
             # optimizer
@@ -120,12 +138,13 @@ class DTN(object):
             self.f_optimizer_src = tf.train.AdamOptimizer(self.learning_rate)
             
             t_vars = tf.trainable_variables()
-            d_vars = [var for var in t_vars if 'discriminator' in var.name]
-            g_vars = [var for var in t_vars if 'generator' in var.name]
-            f_vars = [var for var in t_vars if 'content_extractor' in var.name]
+            d_vars = [var for var in t_vars if 'Discriminator' in var.name]
+            g_vars = [var for var in t_vars if 'Generator' in var.name]
+            f_vars = [var for var in t_vars if 'discriminator' in var.name]
             
             # train op
-            with tf.name_scope('source_train_op'):
+            # with tf.name_scope('source_train_op'):
+            with tf.variable_scope('source_train_op', reuse=False):
                 self.d_train_op_src = slim.learning.create_train_op(self.d_loss_src, self.d_optimizer_src, variables_to_train=d_vars)
                 self.g_train_op_src = slim.learning.create_train_op(self.g_loss_src, self.g_optimizer_src, variables_to_train=g_vars)
                 self.f_train_op_src = slim.learning.create_train_op(self.f_loss_src, self.f_optimizer_src, variables_to_train=f_vars)
@@ -141,16 +160,25 @@ class DTN(object):
                                                     sampled_images_summary])
             
             # target domain (mnist)
-            self.fx = self.content_extractor(self.trg_images, reuse=True)
+            # self.fx = self.content_extractor(self.trg_images, reuse=True)
+            _, _, self.fx = dcgan.discriminator(self.trg_images, reuse=True)
+            print("self.fx is", self.fx)
             self.reconst_images = self.generator(self.fx, reuse=True)
             self.logits_fake = self.discriminator(self.reconst_images, reuse=True)
             self.logits_real = self.discriminator(self.trg_images, reuse=True)
             
             # loss
-            self.d_loss_fake_trg = slim.losses.sigmoid_cross_entropy(self.logits_fake, tf.zeros_like(self.logits_fake))
-            self.d_loss_real_trg = slim.losses.sigmoid_cross_entropy(self.logits_real, tf.ones_like(self.logits_real))
+            # self.d_loss_fake_trg = slim.losses.sigmoid_cross_entropy(self.logits_fake, tf.zeros_like(self.logits_fake))
+            # self.d_loss_real_trg = slim.losses.sigmoid_cross_entropy(self.logits_real, tf.ones_like(self.logits_real))
+
+            self.d_loss_fake_trg = tf.losses.sigmoid_cross_entropy(logits=self.logits_fake, multi_class_labels=tf.zeros_like(self.logits_fake))
+            self.d_loss_real_trg = tf.losses.sigmoid_cross_entropy(logits=self.logits_real, multi_class_labels=tf.ones_like(self.logits_real))
+
             self.d_loss_trg = self.d_loss_fake_trg + self.d_loss_real_trg
-            self.g_loss_fake_trg = slim.losses.sigmoid_cross_entropy(self.logits_fake, tf.ones_like(self.logits_fake))
+
+            # self.g_loss_fake_trg = slim.losses.sigmoid_cross_entropy(self.logits_fake, tf.ones_like(self.logits_fake))
+
+            self.g_loss_fake_trg = tf.losses.sigmoid_cross_entropy(logits=self.logits_fake, multi_class_labels=tf.ones_like(self.logits_fake))
             self.g_loss_const_trg = tf.reduce_mean(tf.square(self.trg_images - self.reconst_images)) * 15.0
             self.g_loss_trg = self.g_loss_fake_trg + self.g_loss_const_trg
             
@@ -159,7 +187,8 @@ class DTN(object):
             self.g_optimizer_trg = tf.train.AdamOptimizer(self.learning_rate)
 
             # train op
-            with tf.name_scope('target_train_op'):
+            # with tf.name_scope('target_train_op'):
+            with tf.variable_scope('target_train_op', reuse=False):
                 self.d_train_op_trg = slim.learning.create_train_op(self.d_loss_trg, self.d_optimizer_trg, variables_to_train=d_vars)
                 self.g_train_op_trg = slim.learning.create_train_op(self.g_loss_trg, self.g_optimizer_trg, variables_to_train=g_vars)
             
