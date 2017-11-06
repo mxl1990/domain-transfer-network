8fe559dccdedc8be0cc14cd80ad2355934866063
mxl1990 <mxl1990@gmail.com>
2017-11-06 15:11:31 +0800

    1.change init function of this class 2.change the way load data of image 3.add intermediate placeholder to implement shuffle the feature maps

--- solver.py
+++ solver.py
@@ -11,11 +11,11 @@ from glob import glob
 
 class Solver(object):
 
-    def __init__(self, sess, model, batch_size=64, pretrain_iter=20000, train_iter=2000, sample_iter=100, 
+    def __init__(self, sess, model, batch_size=64, pretrain_iter=20000, train_iter=20000, sample_iter=100, 
                  source_dir='source', target_dir='target', log_dir='logs', sample_save_path='sample', 
                  model_save_path='model', 
                  pretrained_model='D:\\workspace\\tensorflow-101.git\\DTN\\trunk\\checkpoint\\celebA_64_96_96\\DCGAN.model-18990', 
-                 test_model='model/dtn-2000'):
+                 test_model='model/dtn-20000'):
         
         self.sess = sess
         self.model = model
@@ -30,22 +30,14 @@ class Solver(object):
         self.model_save_path = model_save_path
         self.pretrained_model = pretrained_model
         self.test_model = test_model
-        self.config = tf.ConfigProto()
-        self.config.gpu_options.allow_growth=True
-        self.config.operation_timeout_in_ms = 1000000
+        # self.config = tf.ConfigProto()
+        # self.config.gpu_options.allow_growth=True
+        # self.config.operation_timeout_in_ms = 10000000
 
-
-
-    # def load_source(self, image_dir):
-    #   return self._load_images(image_dir)
-
-    # def load_target(self, image_dir):
-    #   return self._load_images(image_dir)
-
-    def load_images(self, image_dir):
+    def load_images(self, image_dir, name='load_image'):
         import os
         paths = glob(os.path.join(image_dir, "*.jpg"))
-        file_queue = tf.train.string_input_producer(paths, capacity=len(paths))
+        file_queue = tf.train.string_input_producer(paths, capacity=len(paths), name=name)
         images = []
         num_threads = 8
         for _ in range(num_threads):
@@ -54,6 +46,9 @@ class Solver(object):
             image = tf.image.decode_image(image_content)
             image = tf.cast(image, tf.float32)
             image = image / 127.5 - 1
+            # image = tf.reshape(image, [-1, 3])
+            # image = tf.random_shuffle(image)
+            # image = tf.reshape(image, [96, 96, 3])
             images.append([image])
 
         return tf.train.batch_join(
@@ -95,6 +90,8 @@ class Solver(object):
 
     def merge_images(self, sources, targets, k=10):
         _, h, w, _ = sources.shape
+        targets = (targets + 1) * 127.5 
+        sources = (sources + 1) * 127.5
         row = int(np.sqrt(self.batch_size))
         merged = np.zeros([row*h, row*w*2, 3])
 
@@ -145,8 +142,8 @@ class Solver(object):
         # load svhn dataset
         # svhn_images, _ = self.load_svhn(self.svhn_dir, split='train')
         # mnist_images, _ = self.load_mnist(self.mnist_dir, split='train')
-        source_image = self.load_images(self.source_dir)
-        target_image = self.load_images(self.target_dir)
+        source_image = self.load_images(self.source_dir, name='load_source')
+        target_image = self.load_images(self.target_dir, name='load_target')
 
         # build a graph
         model = self.model
@@ -175,6 +172,13 @@ class Solver(object):
             summary_writer = tf.summary.FileWriter(logdir=self.log_dir, graph=tf.get_default_graph())
             saver = tf.train.Saver()
 
+            # load trained model if exist
+            ckpt = tf.train.get_checkpoint_state(self.model_save_path)  
+            if ckpt and ckpt.model_checkpoint_path:
+                print("load trained model")
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print("laod trained model finished")  
+
             print ('start training..!')
             f_interval = 15
             for step in range(self.train_iter+1):
@@ -183,7 +187,16 @@ class Solver(object):
                 trg_images = sess.run(target_image)
                 
 
-                feed_dict = {model.src_images: src_images}
+                fx_dict = {model.src_images: src_images, model.trg_images: trg_images}
+
+                fx_src, fx_trg = sess.run([model.fx_src, model.fx_trg], fx_dict)
+                # print("获取feature map结束")
+
+                src_random, trg_random = fx_src.copy(), fx_trg.copy()
+                np.random.shuffle(src_random)
+                np.random.shuffle(trg_random)
+                feed_dict = {model.trg_images:trg_images, model.src_images: src_images,
+                             model.g_input_src:src_random, model.g_input_trg:trg_random, model.fx_input:fx_src}
                 
                 sess.run(model.d_train_op_src, feed_dict) 
                 sess.run([model.g_train_op_src], feed_dict)
@@ -193,11 +206,11 @@ class Solver(object):
                 sess.run([model.g_train_op_src], feed_dict) 
                 sess.run([model.g_train_op_src], feed_dict)
                 
-                if step > 1600:
-                    f_interval = 30
+                # if step > 1600:
+                #     f_interval = 30
                 
-                if step % f_interval == 0:
-                    sess.run(model.f_train_op_src, feed_dict)
+                # if step % f_interval == 0:
+                #     sess.run(model.f_train_op_src, feed_dict)
                 
                 if (step+1) % 10 == 0:
                     summary, dl, gl, fl = sess.run([model.summary_op_src, \
@@ -207,7 +220,7 @@ class Solver(object):
                                %(step+1, self.train_iter, dl, gl, fl))
                 
                 # train the model for target domain T
-                feed_dict = {model.src_images: src_images, model.trg_images: trg_images}
+                # feed_dict = {model.src_images: src_images, model.trg_images: trg_images}
                 sess.run(model.d_train_op_trg, feed_dict)
                 sess.run(model.d_train_op_trg, feed_dict)
                 sess.run(model.g_train_op_trg, feed_dict)
@@ -221,10 +234,23 @@ class Solver(object):
                     summary_writer.add_summary(summary, step)
                     print ('[Target] step: [%d/%d] d_loss: [%.6f] g_loss: [%.6f]' \
                                %(step+1, self.train_iter, dl, gl))
+                    fx_dict = {model.src_images: src_images}
+                    fx_src = sess.run(model.fx_src, fx_dict)
+                    np.random.shuffle(fx_src)
+                    feed_dict = {model.g_input_src: fx_src}
+                    sampled_batch_images = sess.run(model.fake_images, feed_dict)
+                    # merge and save source images and sampled target images
+                    merged = self.merge_images(src_images, sampled_batch_images)
+                    path = os.path.join(self.sample_save_path, 'sample-%d-to-%d.png' %(step*self.batch_size, (step+1)*self.batch_size))
+                    scipy.misc.imsave(path, merged)
+                    print ('saved %s' %path)
+
 
                 if (step+1) % 200 == 0:
                     saver.save(sess, os.path.join(self.model_save_path, 'dtn'), global_step=step+1)
                     print ('model/dtn-%d saved' %(step+1))
+            coord.request_stop()
+            coord.join(threads)
                 
     def eval(self):
         # build model
@@ -248,10 +274,9 @@ class Solver(object):
             for i in range(self.sample_iter):
                 # train model for source domain S
                 batch_images = sess.run(source_image)
-                print("get batch images")
                 feed_dict = {model.images: batch_images}
                 sampled_batch_images = sess.run(model.sampled_images, feed_dict)
-                print("generate images")
+
 
                 # merge and save source images and sampled target images
                 merged = self.merge_images(batch_images, sampled_batch_images)
