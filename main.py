48ca9a3b03d6c5d215d2ea54ed3ca1d444475f4c
mxl1990 <mxl1990@gmail.com>
2017-11-06 15:04:21 +0800

    1.add more parameter when run py script

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ flags = tf.app.flags
 flags.DEFINE_string('mode', 'train', "'pretrain', 'train' or 'eval'")
 flags.DEFINE_string('model_save_path', 'model', "directory for saving the model")
 flags.DEFINE_string('sample_save_path', 'sample', "directory for saving the sampled images")
+flags.DEFINE_integer("input_height", 96, "input image height")
+flags.DEFINE_integer("input_width", None, "input image width")
+flags.DEFINE_integer("batch_size", 64, "the size of batch")
 FLAGS = flags.FLAGS
 
 def main(_):
-    
-    with tf.Session() as sess:
+    run_config = tf.ConfigProto()
+    # run_config.operation_timeout_in_ms = 100000
+    if FLAGS.input_width is None:
+        FLAGS.input_width = FLAGS.input_height
 
-        model = DTN(mode=FLAGS.mode, learning_rate=0.0003)
-        solver = Solver(sess, model, batch_size=64, pretrain_iter=20000, train_iter=2000, sample_iter=100, 
+    with tf.Session(config=run_config) as sess:
+
+        model = DTN(mode=FLAGS.mode, learning_rate=0.0003, height=FLAGS.input_height, width=FLAGS.input_width, 
+                    batch_size=FLAGS.batch_size)
+        solver = Solver(sess, model, batch_size=FLAGS.batch_size, pretrain_iter=20000, train_iter=20000, sample_iter=100, 
                         source_dir='D:\\workspace\\tensorflow-101.git\\DCGAN\\trunk\\data\\resize', 
                         target_dir='D:\\workspace\\tensorflow-101.git\\DCGAN\\trunk\\data\\faces', 
                         model_save_path=FLAGS.model_save_path, sample_save_path=FLAGS.sample_save_path)
